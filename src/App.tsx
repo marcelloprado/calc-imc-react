634f131b17b555990256aeac5910f54a253b3e12
Marcello Prado <marcellopradomuller@gmail.com>
2023-04-26 21:12:43 -0300

    Iniciando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Styles from "./App.module.css";
+import poweredImage from './assets/powered.png'
 
-function App() {
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightFiel, setWeightField] = useState<number>(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={Styles.main}>
+      <header>
+        <div className={Styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150} />
+        </div>
       </header>
+      <div className={Styles.container}>
+      <div className={Styles.leftSide}>
+        <h1>Calcule o seu IMC.</h1>
+        <p>IMC é a sigle para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+
+        <input 
+          type="number"
+          placeholder="Digite a sua altura. Ex: 1.5 (em métros)"
+          value={}
+          onChange={}
+        />
+      </div>
+      <div className={Styles.rightSide}>
+        RRR
+      </div>
+      </div>
+
     </div>
   );
 }
 
-export default App;
+export default App
